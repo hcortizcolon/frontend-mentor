add0f815da8da4e7c272c2a05085178d39dd4298
Hector <hcortizcolon@fastmail.com>
2025-02-14 07:52:50 -0400

    qr fixes

--- src/app/qr-code/page.tsx
+++ src/app/qr-code/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function QrCode() {
   return (
-    <div className="bg-qr-code-slate-300 w-screen h-screen flex items-center justify-center">
+    <main className="bg-qr-code-slate-300 w-screen h-screen flex items-center justify-center">
       <div className="bg-white w-72 rounded-2xl flex flex-col items-center gap-4 p-4">
         <div className="">
           <Image
@@ -13,14 +13,14 @@ export default function QrCode() {
             className="rounded-lg"
           />
         </div>
-        <div className="font-semibold mt-4 text-black text-xl text-center">
+        <h1 className="font-semibold mt-4 text-black text-xl text-center">
           Improve your front-end skills by building projects
-        </div>
+        </h1>
         <div className="text-center text-sm text-qr-code-slate-500 mt-2">
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
         </div>
       </div>
-    </div>
+    </main>
   );
 }
